6529dbea0a13800a6112c6d8ca81a57dc21b70d3
Sam Olaogun <sam.olaogun@gmail.com>
2017-05-29 15:11:23 -0400

    Fix gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo -n "Commit message: "
+
+read message
+
+git add .
+git commit -m "$message"
+git push -u origin master
+
+# npm version patch
+# npm publish
+
+echo "Deployment complete."
