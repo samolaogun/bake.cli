c17bf2de200aefadc908e217cd467757f1ccbfd2
Sam Olaogun <sam.olaogun@gmail.com>
2017-05-29 22:08:28 -0400

    Stable

--- dist/bake.cli.js
+++ dist/bake.cli.js
@@ -5,7 +5,6 @@
  * bake.cli
  * 
  * @author  Sam Olaogun
- * @since   1.0.0
  * @license MIT
  */
 'use strict';
@@ -27,10 +26,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 process.on('uncaughtException', function (e) {
-    _fs2.default.writeSync(1, 'bake.core: "' + e + '"\n');
+    return _fs2.default.writeSync(1, 'bake.core: "' + e + '"\n');
 });
 
-_commander2.default.version('1.0.1').usage('[options] [sourceFile [,outputFile]]').option('-f, --format', 'unformatted document').option('-p, --prolog', 'remove prolog').option('-pa, --parent [name]', 'add wrapper with name').option('-s, --strict', 'strict content parsing').option('-ai, --attribute-indentifier [name]', 'set content identifier').option('-ci, --content-identifier [name]', 'set content identifier').parse(process.argv);
+var parent = function parent(arg) {
+    return _commander2.default.parent = JSON.parse(arg);
+};
+
+_commander2.default.version('1.0.1').usage('[options] [sourceFile [,outputFile]]').option('-p, --parent [json]', 'top level parent obj', parent).option('-a, --attr', 'enable attribute-content syntax').option('-a, --attribute-indentifier [name]', 'set content identifier').option('-c, --content-identifier [name]', 'set content identifier').option('-s, --strict', 'strict content parsing').option('-u, --unformat', 'unformatted document').option('-l, --prolog [prolog]', 'specify prolog').parse(process.argv);
 
 var result = (0, _bake2.default)(_commander2.default).apply(undefined, _toConsumableArray(_commander2.default.args.splice(0, 2)));
 if (result) _fs2.default.writeSync(1, result + '\n');
